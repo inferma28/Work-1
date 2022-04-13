5e25a6fa865880a5edf2d71e9e5c7cafa717715a
Vova <example@gmail.com>
2022-04-13 15:01:25 +0300

    Lessons 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log(2 + 2 * 2 == 8);
+const numberOfFilms = prompt("Сколько фильмов Вы посмотрели?");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    geners: [],
+    privat: false
+};
+
+const a = prompt ("Один из просмотренных фильмов?", ""),
+    b = prompt ("На сколько оцените его?", ""),
+    c = prompt ("Один из просмотренных фильмов?", ""),
+    d = prompt ("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
